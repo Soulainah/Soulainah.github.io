bf34f05a0f3563968263bfed31dde39e81c41e0d
Soulainah <Soulainah>
2021-04-13 13:33:13 +0200

    ajout du projet Ki Pou Cui

---
+++ KiPouCui2/main.js
@@ -0,0 +1,24 @@
+/*========NAVBAR=======*/
+$(function () {
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() > 10 ) {
+            $('.navbar').addClass('active');
+        } else {
+            $('.navbar').removeClass('active');
+        }
+    });
+});
+/*==================Bouton UP==================*/
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
